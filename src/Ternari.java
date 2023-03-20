6d9f3e227c6e244ef6ccebb75ab73e79b6d1c357
Assami Muzaki <muzakiassami@gmail.com>
2023-03-20 22:02:43 +0700

    adding ternari

---
+++ src/Ternari.java
@@ -0,0 +1,8 @@
+public class Ternari {
+    public static void main(String[] args) {
+        var nilai = 90;
+        var ucapan = nilai > 75 ? "anda lulus" : "anda gagal";
+
+        System.out.println(ucapan);
+    }
+}
