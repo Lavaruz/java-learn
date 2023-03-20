ba7951bad034ea529638b2599bfcd64cefa689c5
Assami Muzaki <muzakiassami@gmail.com>
2023-03-20 19:32:12 +0700

    adding number, string and char data type

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//          Tipe Data Number
+        byte iniByte = 10;
+        short iniShort = 100;
+        int iniInt = 1000;
+        long iniLong = 10000;
+        System.out.println(iniByte);
+
+//          Tipe data String
+        String iniString = "Test String";
+        char iniChar = 'C';
+        System.out.println(iniString);
+        System.out.println(iniChar);
+
+        System.out.println("Hello World");
     }
 }
