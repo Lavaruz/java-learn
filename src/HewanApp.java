8fd8dd1053227f85295be59599bd3589d17ee445
Assami Muzaki <muzakiassami@gmail.com>
2023-03-21 22:15:06 +0700

    adding inheritance (extends)

--- src/HewanApp.java
+++ src/HewanApp.java
@@ -2,5 +2,9 @@ public class HewanApp {
     public static void main(String[] args) {
         var hewan = new Hewan("Jackob");
         hewan.sayHello();
+
+        var hewanDarat = new HewanDarat("Dori");
+        hewanDarat.sayHello();
+        hewanDarat.hidupDimana("Didin");
     }
 }
---
+++ src/HewanDarat.java
@@ -0,0 +1,9 @@
+public class HewanDarat extends Hewan{
+    HewanDarat(String name){
+        super(name);
+    }
+
+    void hidupDimana(String name){
+        System.out.println("aku "+ this.name+" hidup di rumah " + name);
+    }
+}
