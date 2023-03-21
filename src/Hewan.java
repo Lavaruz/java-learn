8453ae1bb73042150e220c33a6ee602f1d0498fc
Assami Muzaki <muzakiassami@gmail.com>
2023-03-21 22:09:52 +0700

    adding method

---
+++ src/Hewan.java
@@ -0,0 +1,11 @@
+public class Hewan {
+    String name;
+
+    Hewan(String name){
+        this.name = name;
+    }
+
+    void sayHello(){
+        System.out.println("hello iam " + this.name);
+    }
+}
---
+++ src/HewanApp.java
@@ -0,0 +1,6 @@
+public class HewanApp {
+    public static void main(String[] args) {
+        var hewan = new Hewan("Jackob");
+        hewan.sayHello();
+    }
+}
